8cc5d28bb9104ac839ea8c0215402baa879effb7
Michael Adams <michael@michaeladams.org>
2021-05-11 14:17:02 +1200

    Fix rack config

--- lib/utopia_site_helpers/rspec/website.rb
+++ lib/utopia_site_helpers/rspec/website.rb
@@ -3,9 +3,9 @@ require 'rack/test'
 RSpec.shared_context "website" do
 	include Rack::Test::Methods
 
-	let(:rackup_path) {File.expand_path('../config.ru', __dir__)}
-	let(:rackup_directory) {File.dirname(rackup_path)}
+	let(:rackup_path) { "#{RSpec::Core::RubyProject.root}/config.ru" }
+	let(:rackup_directory) { File.dirname(rackup_path) }
 
-	let(:app) {Rack::Builder.parse_file(rackup_path).first}
+	let(:app) { Rack::Builder.parse_file(rackup_path).first }
 	let(:host) { [Capybara.current_session.server.host, Capybara.current_session.server.port || 80].join(':') }
 end
