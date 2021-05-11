f562e3b9892ed91bbf19622c2332f975b9bdd80e
Michael Adams <michael@michaeladams.org>
2021-05-11 14:16:49 +1200

    Use testing, not test as variant name

--- lib/utopia_site_helpers/rspec.rb
+++ lib/utopia_site_helpers/rspec.rb
@@ -1,5 +1,7 @@
+require 'variant'
+
 # Ensure that we're always running as the test variant
-ENV['UTOPIA_VARIANT'] = 'test'
+Variant.force!(:testing)
 
 # Set up environment, e.g. libraries, DB, config etc
 require_relative "#{RSpec::Core::RubyProject.root}/config/environment"
